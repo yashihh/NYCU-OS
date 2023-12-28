06ef9e19d016db5d2f1033936a8c68911bee0f6e
yashihh <yashihh.11@nycu.edu.tw>
2023-12-28 23:44:04 +0800

    modified:   kfetch_mod_311581039.c - fetch system info done new file:   run.sh

--- kernel_module/kfetch_mod_311581039.c
+++ kernel_module/kfetch_mod_311581039.c
@@ -1,20 +1,40 @@
-#include <linux/module.h>
+#include <linux/module.h> /* module reference counter */
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/uaccess.h> /* for get_user and put_user */ 
 #include <linux/fs.h> /* register */ 
 #include <linux/device.h> /* device create */
-#include <linux/printk.h> 
+#include <linux/printk.h>
+#include <linux/mm.h> /* si_meminfo() */
+#include <linux/sysinfo.h> /* to get system info. e.g. cpu, mem*/
+#include <linux/utsname.h> /* to get system info. e.g. nodename, release */
+#include <linux/jiffies.h>
 #include "kfetch.h"
+#include <linux/ktime.h>
 
+char pic0[] = "                   ";
+char pic1[] = "        .-.        ";      
+char pic2[] = "       (.. |       ";     
+char pic3[] = "       <>  |       ";     
+char pic4[] = "      / --- \\      ";
+char pic5[] = "     ( |   | |     ";
+char pic6[] = "   |\\\\_)___/\\)/\\   ";
+char pic7[] = "  <__)------(__/   ";
+char dash[] = "-------------";
 
-char pic1[] = "        .-.";      
-char pic2[] = "       (.. |";     
-char pic3[] = "       <>  |";     
-char pic4[] = "      / --- \\";
-char pic5[] = "     ( |   | |";
-char pic6[] = "   |\\\\_)___/\\)/\\";
-char pic7[] = "  <__)------(__/";
+char info0[] = "\0";
+char info1[] = "\0";
+char info2[] = "\0";
+char info3[] = "\0";
+char info4[] = "\0";
+char info5[] = "\0";
+
+char sys_kernel[30];
+char sys_cpu[50];
+char sys_cpus[10];
+char sys_mem[20];
+char sys_procs[10];
+char sys_uptime[10];
 
 /* Global variables are declared as static, so are global within the file. */ 
 
@@ -23,7 +43,7 @@ static int major; /* major number assigned to our device driver */
 /* Is device open? Used to prevent multiple access to device */ 
 //static atomic_t already_open = ATOMIC_INIT(CDEV_NOT_USED); 
  
-static char kfetch_buf[KFETCH_BUF_SIZE + 1]; 
+static char kfetch_buf[KFETCH_BUF_SIZE]; 
 
 static struct class *cls;
 
@@ -38,16 +58,57 @@ static ssize_t kfetch_read(struct file *filp,
                            loff_t *offset)
 {
     pr_info("kfetch_read\n");
+    int len;
+    struct cpuinfo_x86 *cpuinfo = &cpu_data(0);
+    struct sysinfo i;
+    si_meminfo(&i);
+    
+    s64  uptime;
+    uptime = ktime_to_ms(ktime_get_boottime()) / 1000;
+    unsigned int runningProcs = 0;
 
+    struct task_struct *task;
+    for_each_process(task) {
+        runningProcs++;
+    }
+    sprintf(sys_kernel, "%s", init_utsname()->release);
+    sprintf(sys_cpu, "%s", cpuinfo->x86_model_id );
+    sprintf(sys_cpus, "%lu / %lu", num_online_cpus(), num_present_cpus());
+    sprintf(sys_mem, "%lu MB / %lu MB", i.freeram * 4 / 1024, i.totalram * 4 / 1024 );
+    sprintf(sys_procs, "%u", runningProcs );
+    sprintf(sys_uptime, "%lu mins", uptime / 60);
+    len = sprintf(kfetch_buf, 
+                    "%s%s\n"
+                    "%s%s\n"
+                    "%s%s\n"
+                    "%s%s\n"
+                    "%s%s\n"
+                    "%s%s\n"
+                    "%s%s\n"
+                    "%s%s\n",
+                    pic0, init_utsname()->nodename,
+                    pic1, dash, 
+                    pic2, sys_kernel,
+                    pic3, sys_cpu,
+                    pic4, sys_cpus,
+                    pic5, sys_mem,
+                    pic6, sys_procs,
+                    pic7, sys_uptime);
     /* fetching the information */
 
-    if (copy_to_user(buffer, kfetch_buf, length)) {
+
+    if (copy_to_user(buffer, kfetch_buf, len)) {
         pr_alert("Failed to copy data to user");
         return 0;
     }
-    return 0;
-    
+
     /* cleaning up */
+    memset(kfetch_buf, 0, KFETCH_BUF_SIZE);
+
+    /* Read functions are supposed to return the number of bytes actually 
+     * inserted into the buffer. 
+     */ 
+    return len;
 }
 
 /* Called when a process writes to dev file: echo "hi" > /dev/hello */ 
@@ -56,8 +117,7 @@ static ssize_t kfetch_write(struct file *filp,
                             size_t length,
                             loff_t *offset)
 {
-    pr_info("kfetch_write\n");
-
+    //memcpy(array2, array1, sizeof(array2));
     int mask_info;
 
     if (copy_from_user(&mask_info, buffer, length)) {
@@ -66,6 +126,26 @@ static ssize_t kfetch_write(struct file *filp,
     }
 
     /* setting the information mask */
+    if (mask_info & KFETCH_RELEASE) {
+
+    }
+    if (mask_info & KFETCH_NUM_CPUS) {
+
+    }
+    if (mask_info & KFETCH_CPU_MODEL) {
+
+    }
+    if (mask_info & KFETCH_MEM) {
+
+    }
+    if (mask_info & KFETCH_UPTIME) {
+
+    }
+    if (mask_info & KFETCH_NUM_PROCS) {
+
+    }
+
+    pr_info("[kfetch_write] mask_info: [%d]\n", mask_info);
     return 0;
 
 }
@@ -75,14 +155,14 @@ static ssize_t kfetch_write(struct file *filp,
  */ 
 static int kfetch_open(struct inode *inode, struct file *filp) 
 {
-    try_module_get(THIS_MODULE);
+    try_module_get(THIS_MODULE); // Increment the reference count
     return 0;
 }
 
 /* Called when a process closes the device file. */ 
 static int kfetch_release(struct inode *inode, struct file *filp)
 {
-    pr_info("kfetch_release\n");
+    module_put(THIS_MODULE); // Decrement the reference count
     return 0;
 }
 
---
+++ kernel_module/run.sh
@@ -0,0 +1,4 @@
+make clean
+make
+make unload
+make load
